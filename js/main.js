17ddea932752804fcbf9e264853afa5cd632b206
ilhamgumawang <ilhamgumawang29@gmail.com>
2022-07-31 16:14:06 +0700

    profile

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var typed = $(".typed");
 
 $(function () {
   typed.typed({
-    strings: ["Ilham Risan Gumawang.", "Programmer."],
+    strings: ["Ilham Risan Gumawang.", "student.", "employee."],
     typeSpeed: 100,
     loop: true,
   });
